6e8b411809bf174de4d724c4b943b6cf26ecd402
hadi <hadi.abou.mrad@gmail.com>
2025-08-16 18:23:48 +0200

    print http error details to help user understand

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 #!/usr/bin/env node
 import { CommandLineModule } from './application/command-line/command-line.module';
 import { CommandFactory } from 'nest-commander';
+import axios, { AxiosError } from 'axios';
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error instanceof AxiosError) {
+      console.error(
+        `Error. Method: ${error.config.method}, URL: ${
+          error.config.url
+        }, Status: ${error.response?.status}, Message: ${
+          error.message
+        }, Request Data: ${
+          error.config.data ? JSON.stringify(error.config?.data) : undefined
+        }, Response Data: ${
+          error.response?.data
+            ? JSON.stringify(error.response?.data)
+            : undefined
+        }`,
+      );
+    }
+  },
+);
 
 async function bootstrap() {
   await CommandFactory.run(CommandLineModule, ['log', 'warn', 'error']);
